483923a032979c76bbb0790ecea5e6bac908be06
Ritik Raj <raj769417@gmail.com>
2023-09-15 20:07:02 +0530

    implemented document deletion after certain time

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const generateUniqueId = require("generate-unique-id");
+
+const Paste = require("./models/paste");
 
 const app = express();
 
-const paste_schema = new mongoose.Schema({
-  text: String,
-  address: String,
-  createdAt: {
-    type: Date,
-    default: () => {
-      const now = new Date();
-      now.setHours(now.getHours() + 5);
-      now.setMinutes(now.getMinutes() + 30);
-      return now;
-    },
-  },
-});
-
-paste_schema.pre("save", function (next) {
-  if (!this.address) {
-    const pasteAddres = generateUniqueId({
-      length: 6,
-      useLetters: true,
-    });
-    this.address = pasteAddres;
-    console.log(this.address);
-  }
-  next();
-});
-
-const Paste = mongoose.model("Paste", paste_schema);
-
 mongoose
-  .connect("mongodb://127.0.0.1:27017/paste-service")
-  .then((data) => {
-    console.log("CONNECTION ESTABLISHED.");
-  })
-  .catch((err) => {
-    console.log("Error while connecting !!!");
-  });
+    .connect("mongodb://127.0.0.1:27017/paste-service")
+    .then((data) => {
+        console.log("CONNECTION ESTABLISHED.");
+    })
+    .catch((err) => {
+        console.log("Error while connecting !!!");
+    });
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -47,33 +20,37 @@ app.use(express.urlencoded({ extended: true }));
 PORT = 3000;
 
 app.get("/", async (req, res) => {
-  res.render("home");
+    res.render("home");
 });
 
 app.post("/", async (req, res) => {
-  const { text } = req.body;
-  const Text = new Paste({ text });
+    const { text } = req.body;
+    const Text = new Paste({ text });
 
-  await Text.save();
+    await Text.save();
 
-  res.redirect(`/view/${Text.address}`);
+    res.redirect(`/view/${Text.address}`);
 });
 
-app.get('/view/:id', async (req, res) => {
-  const { id } = req.params;
+app.get("/view/:id", async (req, res) => {
+    const { id } = req.params;
 
-  const Text = await Paste.findOne({ address: id });
-  console.log(Text);
+    const Text = await Paste.findOne({ address: id });
+    console.log(Text);
 
-  if(!Text) {
-    console.log("Cannot find paste link");
-    return res.send('Error');
-  }
-  res.render('view', { Text });
-})
+    if (!Text) {
+        console.log("Cannot find paste link");
+        return res.send("Error");
+    }
 
-// http://localhost:3000/view/d132t6
+    const isAlive = await Text.alive();
+    if (isAlive) {
+        return res.render("view", { Text });
+    }
+
+    return res.send("Expired");
+});
 
 app.listen(3000, () =>
-  console.log(`LISTENING ON PORT 3000\nhttp://localhost:${3000}`)
+    console.log(`LISTENING ON PORT 3000\nhttp://localhost:${3000}`)
 );
---
+++ models/paste.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const generateUniqueId = require("generate-unique-id");
+
+const paste_schema = new mongoose.Schema({
+    text: String,
+    address: String,
+    createdAt: {
+        type: Date,
+        default: () => {
+            const now = new Date();
+            now.setHours(now.getHours() + 5);
+            now.setMinutes(now.getMinutes() + 30);
+            return now;
+        },
+    },
+    expiresAt: {
+        type: Date,
+        default: () => {
+            const now = new Date(Date.now() + 2 * 60 * 1000);
+            now.setHours(now.getHours() + 5);
+            now.setMinutes(now.getMinutes() + 30);
+            return now;
+        },
+    },
+});
+
+paste_schema.pre("save", function (next) {
+    if (!this.address) {
+        const pasteAddres = generateUniqueId({
+            length: 6,
+            useLetters: true,
+        });
+        this.address = pasteAddres;
+        console.log(this.address);
+    }
+    next();
+});
+
+// instance method to check if document has expired
+paste_schema.methods.alive = async function () {
+    const expiresAt = this.expiresAt;
+
+    // converting current time to indian time we have to 
+    // offset it by 5:30 => 19800000 ms
+    const current_time = new Date(Date.now() + 19800000);
+
+    if (expiresAt <= current_time) {
+        await this.deleteOne();
+        return false;
+    }
+    return true;
+};
+
+const Paste = mongoose.model("Paste", paste_schema);
+
+module.exports = Paste;
